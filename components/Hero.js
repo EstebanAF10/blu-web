11acdb185de2eaa8a38b8bbcf01b6f1d405d1299
Esteban Arias <esteban.arfu@icloud.com>
2022-09-02 20:27:49 -0600

    Update1

---
+++ components/Hero.js
@@ -0,0 +1,13 @@
+app.component("hero-section", {
+    props: {},
+    methods: {
+  
+    },
+    template:
+      /*html*/
+      `<div>
+      <img class="botella-vidrio mx-auto d-block" src="./imgs/botella-vidrio-sombra.png" alt="Botella Vidrio Agua BLU"/>
+      <h1 class="hero-text">ALKALINE WATER</h1>
+      </div>
+      `,
+  });
