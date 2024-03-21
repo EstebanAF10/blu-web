3ad6cc6f9e241de18929414398d05f210ded6e0f
Esteban <esteban.arfu@gmail.com>
2024-03-21 15:18:43 -0600

    Hero and Navbar

--- src/components/discover/index.jsx
+++ src/components/discover/index.jsx
@@ -10,6 +10,7 @@ const Discover = () => {
       <h1 className="text-black font-bold text-[50px] font-inter w-[1050px]">Descubrí los beneficios<br/>de Agua BLU</h1>
       <h1 className="text-black font-light text-[20px] font-inter w-[800px]">BLU is not just any ordinary water. With its rich mineral composition and alkaline pH, it offers numerous health benefits. Drinking BLU can help balance the body's pH levels, support hydration, and promote overall well-being.</h1>
     </div>
+    
     </>
   );
 };
